c5cf26cae2ffdbaa62de9498c675da8d126e70f1
Demski <97314528+DemskiIs@users.noreply.github.com>
2022-03-31 11:28:18 +0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadingMode: boolean = true 
+  loadingMode: boolean = false 
   title = 'crocobet';
 
   togglePreloader(isLoading: boolean) {
